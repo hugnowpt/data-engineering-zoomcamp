ac66fb7cc94c0156a16e7643c3d232e380f1cf4e
phetnaphan aphichokphalakorn <phetnaphan.apt@gmail.com>
2026-02-16 16:26:50 +0000

    Run Model

--- 04-analytics-engineering/models/marts/dim_zones.sql
+++ 04-analytics-engineering/models/marts/dim_zones.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 -- Dimension table for NYC taxi zones
 -- This is a simple pass-through from the seed file, but having it as a model
 -- allows for future enhancements (e.g., adding calculated fields, filtering)
@@ -8,15 +8,4 @@ select
     borough,
     zone,
     service_zone
-=======
--- Dimension table for NYC taxi zones
--- This is a simple pass-through from the seed file, but having it as a model
--- allows for future enhancements (e.g., adding calculated fields, filtering)
-
-select
-    locationid as location_id,
-    borough,
-    zone,
-    service_zone
->>>>>>> a87db69c4b64cd09b9ae6cdc8ac36ab2587a1015
 from {{ ref('taxi_zone_lookup') }}
