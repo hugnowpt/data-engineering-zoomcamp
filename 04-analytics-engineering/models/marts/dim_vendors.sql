1276b4b09ccce8c8ceb6b1c35152273e191d0cd3
phetnaphan aphichokphalakorn <phetnaphan.apt@gmail.com>
2026-02-16 16:26:50 +0000

    Run Model

--- 04-analytics-engineering/models/marts/dim_vendors.sql
+++ 04-analytics-engineering/models/marts/dim_vendors.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 -- Dimension table for taxi technology vendors
 -- Small static dimension defining vendor codes and their company names
 
@@ -14,20 +14,3 @@ vendors as (
 )
 
 select * from vendors
-=======
--- Dimension table for taxi technology vendors
--- Small static dimension defining vendor codes and their company names
-
-with trips as (
-    select * from {{ ref('fct_trips') }}
-),
-
-vendors as (
-    select distinct
-        vendor_id,
-        {{ get_vendor_data('vendor_id') }} as vendor_name
-    from trips
-)
-
-select * from vendors
->>>>>>> a87db69c4b64cd09b9ae6cdc8ac36ab2587a1015
